c7e577dba56f99278376777717e4fb0b3e41bf69
Akeem King <akeemtlking@gmail.com>
2024-12-12 09:14:02 -0500

    Updating test.py

--- test.py
+++ test.py
@@ -1,11 +1,5 @@
 import httpx
 import urllib
-
-#v = 'https://www.deviantart.com/muse1908'
-#v = 'https://twitter.com/MynameisAkeem'
-#v = 'http://web.archive.org/web/20230326054832/https://twitter.com/MynameisAkeem'
-
 v = 'https://www.deviantart.com/muse1908'
 url = urllib.parse.quote(v, safe='')
 response = httpx.get(f'http://localhost:8000/profile?url={url}')
-print(response.json())
