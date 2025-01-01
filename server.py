704432f39ec2c4c63f225056e3416db8d23612d6
Akeem King <akeemtlking@gmail.com>
2025-01-01 16:36:45 -0500

    Addings CORS middleware

--- server.py
+++ server.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .main import search_profile
 
 app = FastAPI()
 
-@app.get('/profile')
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+@app.get('/focus')
 def find_profile(url: str):
     profile_info = search_profile(url)
     return profile_info
